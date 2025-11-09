834fda1533db7bd6d32333a46645dcc2da83578c
riskinurhadi <rizkibinmangtrisno@gmail.com>
2025-11-09 23:19:01 +0700

    modified:   index.php

--- index.php
+++ index.php
@@ -131,7 +131,7 @@
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNavbar">
         <div class="container">
             <a class="navbar-brand d-flex align-items-center" href="#home">
-                <img src="img/logobus.png" alt="Sewa Bus Jogja" height="90" class="me-2">
+                <img src="img/logobus.png" alt="Sewa Bus Jogja" height="50" class="me-2">
                 <span class="brand-text"></span>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
