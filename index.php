51c9ea3c68ad45de132990f3501b6048c86eaf84
riskinurhadi <rizkibinmangtrisno@gmail.com>
2025-11-18 21:41:25 +0700

    modified:   index.php

--- index.php
+++ index.php
@@ -139,6 +139,7 @@
         <div class="container">
             <a class="navbar-brand" href="#home">
                 <?php echo getMediaImage('logo_navbar', 'img/logobus.png', 'Sewa Bus Jogja - Rental Bus Yogyakarta', 'navbar-logo'); ?>
+                <h2>PT KRESNA UTAMA TRANS WISATA</h2>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
